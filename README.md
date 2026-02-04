ae05eb5ee629ac5232f7da129918a5e006983ded
John Saigle <nothanks>
2026-02-04 07:59:55 -0500

    remove fabric

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # scaffold
 
-## Scripts
-
-The fetch-descriptions.sh script is used to fetch the descriptions of fabric patterns and format them such that the can
-be read by LLMs easily.
--- scripts/fetch_descriptions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Fetch the pattern descriptions from the main branch of the Fabric repo
-curl --silent -L -O https://raw.githubusercontent.com/danielmiessler/Fabric/refs/heads/main/scripts/pattern_descriptions/pattern_descriptions.json
-
-# Format
-cat pattern_descriptions.json |jq -r '.patterns[] | "\(.patternName):\(.description)"' > FABRIC.md
